685d30399ef4f422eebeb876ab577fa92aa1f033
Juan Felipe Marin <102315395+Marin02@users.noreply.github.com>
2023-06-19 21:46:18 -0500

    Uploaded.

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+let familia = ['Fernando', 'Luz Elena', 'Santiago', 'Felipe'];
+
+let setFamilia = new Set(familia)
+
+setFamilia.add('Felipe')
+setFamilia.add('Javascript')
+
+console.log(setFamilia)
